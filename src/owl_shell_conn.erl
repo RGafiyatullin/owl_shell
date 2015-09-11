36a465f253a91d00497bf0e29248ba75b4e72c53
Roman Gafiyatullin <r.gafiyatullin@me.com>
2015-09-11 19:56:33 +0300

    owl_shell_conn is a gen_server now

--- src/owl_shell_conn.erl
+++ src/owl_shell_conn.erl
@@ -1,6 +1,17 @@
 -module (owl_shell_conn).
+-behaviour (gen_server).
+
 -export ([start_link/3, post_result/4, post_error/5]).
 -export ([init_it/3]).
+
+-export ([
+		init/1,
+		handle_call/3,
+		handle_cast/2,
+		handle_info/2,
+		terminate/2,
+		code_change/3
+	]).
 
 -define(io, owl_shell_conn_io).
 -define(post_result( SID, RID, Value ), { post_result, SID, RID, Value }).
@@ -10,12 +21,10 @@
 start_link( TopSup, ConnSupSup, ConnSup ) -> proc_lib:start_link( ?MODULE, init_it, [ TopSup, ConnSupSup, ConnSup ] ).
 
 post_result( Connection, SID, RID, Result ) ->
-	_ = erlang:send( Connection, ?post_result( SID, RID, Result ) ),
-	ok.
+	ok = gen_server:cast( Connection, ?post_result( SID, RID, Result ) ).
 
 post_error( Connection, SID, RID, EvalProcessDead, Reason ) ->
-	_ = erlang:send( Connection, ?post_error( SID, RID, EvalProcessDead, Reason ) ),
-	ok.
+	ok = gen_server:cast( Connection, ?post_error( SID, RID, EvalProcessDead, Reason ) ).
 
 -record( s, {
 		'#module' = ?MODULE :: ?MODULE,
@@ -28,6 +37,8 @@ post_error( Connection, SID, RID, EvalProcessDead, Reason ) ->
 		active_session :: pid() | undefined,
 		active_session_id :: undefined | integer()
 	} ).
+
+init( _ ) -> {stop, enter_loop_used}.
 
 init_it( TopSup, ConnSupSup, ConnSup ) ->
 	ok = proc_lib:init_ack( {ok, self()} ),
@@ -44,88 +55,65 @@ init_it( TopSup, ConnSupSup, ConnSup ) ->
 			active_session_id = SID,
 			active_session = Session
 		},
+
 	{ok, S1} = io_write_prompt( S0 ),
-
-	eval_loop( S1 ).
-
-eval_loop( S0 = #s{ io = IO } ) ->
-	receive
-		{?io, data, IO, Data} ->
-			handle_io_data( Data, S0 );
-
-		{?io, closed, IO} ->
-			handle_io_closed( S0 );
-
-		?post_result( SID, RID, Result ) ->
-			?io:write( IO, io_lib:format( "v(~p:~p) = ~p~n", [ SID, RID, Result ]) ),
-			{ok, S1} = io_write_prompt( S0 ),
-			eval_loop( S1 );
-
-		?post_error( SID, RID, EvalProcessDead, Reason ) ->
-			?io:write( IO, io_lib:format( "v(~p:~p) : ~p terminated with exception~n", [ SID, RID, EvalProcessDead ] ) ),
-			?io:write( IO, io_lib:format( "\t~p~n", [ Reason ] ) ),
-			{ok, S1} = io_write_prompt( S0 ),
-			eval_loop( S1 );
-
-		{io_request, ReplyTo, ReplyAs, Request} ->
-			case Request of
-				{ put_chars, _Enc, Chars } ->
-					?io:write( IO, Chars );
-				{ put_chars, Chars } ->
-					?io:write( IO, Chars );
-
-				{ put_chars, _Enc, M, F, A } ->
-					?io:write( IO, erlang:apply( M, F, A ) );
-				{ put_chars, M, F, A } ->
-					?io:write( IO, erlang:apply( M, F, A ) );
+	gen_server:enter_loop(?MODULE, [], S1).
 
 
-				_ ->
-					?io:write( IO, io_lib:format( "UNKNOWN-IO-REQUEST: ~p~n", [ Request ] ) )
-			end,
-			_ = erlang:send( ReplyTo, {io_reply, ReplyAs, ok} ),
-			eval_loop( S0 );
 
-		Rubbish ->
-			io:format("Recvd rubbish: ~p~n", [ Rubbish ]),
-			eval_loop( S0 )
-	end.
+handle_call( _Unexpected, _GenReplyTo, S ) -> {reply, badarg, S}.
+
+handle_cast( ?post_result( SID, RID, Result ), S ) ->
+	handle_cast_post_result( SID, RID, Result, S );
+handle_cast( ?post_error( SID, RID, EvalProcessDead, Reason ), S ) ->
+	handle_cast_post_error( SID, RID, EvalProcessDead, Reason, S );
+handle_cast( _Unexpected, S ) -> {noreply, S}.
+
+handle_info( {?io, data, IO, Data}, S ) ->
+	handle_info_io_data( IO, Data, S );
+handle_info( {?io, closed, IO}, S ) ->
+	handle_info_io_closed( IO, S );
+
+handle_info( {io_request, ReplyTo, ReplyAs, Request}, S ) ->
+	handle_info_io_request( ReplyTo, ReplyAs, Request, S );
+
+handle_info( _Unexpected, S ) -> {noreply, S}.
+
+terminate( _Reason, _S ) -> ignore.
+
+code_change( _OldVsn, S, _Extra ) -> {ok, S}.
 
 
-handle_io_closed( #s{ conn_sup_sup = SupSup, conn_sup = Sup } ) ->
+
+
+
+
+handle_info_io_closed( IO, _S = #s{ io = IO, conn_sup_sup = SupSup, conn_sup = Sup } ) ->
 	_ = supervisor:terminate_child( SupSup, Sup ).
 
-
-handle_io_data( Data, S0 = #s{ io = _IO, token_q = TokQ0 } ) ->
-	% io:format("DataIn: ~p~n", [ Data ]),
-	% ok = ?io:write( IO, <<"okay, keep talking...\n">> ),
-
+handle_info_io_data( IO, Data, S0 = #s{ io = IO, token_q = TokQ0 } ) ->
 	case erl_scan:string( binary_to_list( Data ) ) of
 		{ok, Tokens, _EndPos} ->
-			% io:format("\tTokens: ~p~n", [ Tokens ]),
 			TokQ1 = lists:foldl( fun queue:in/2, TokQ0, Tokens ),
-			handle_io_data_process_token_queue( S0 #s{ token_q = TokQ1 } );
+			handle_info_io_data_process_token_queue( S0 #s{ token_q = TokQ1 } );
+
 		{error, _ErrorInfo, _ErrorLocation} ->
-			% io:format("\tError: ~p (~p)~n", [ ErrorInfo, ErrorLocation ]),
-			eval_loop( S0 )
+			{noreply, S0}
 	end.
 
-
-handle_io_data_process_token_queue( S0 = #s{ token_q = TokQ0, active_session = Session } ) ->
+handle_info_io_data_process_token_queue( S0 = #s{ token_q = TokQ0, active_session = Session } ) ->
 	case token_queue_get_expr( TokQ0 ) of
-		error -> eval_loop( S0 );
-
+		error -> {noreply, S0};
 		{ok, ExprTokens, TokQ1} ->
 			S1 = S0 #s{ token_q = TokQ1 },
 			case erl_parse:parse_exprs( ExprTokens ) of
 				{ok, Exprs} ->
-					% io:format("\t\tExprs: ~p~n", [ Exprs ]),
 					ok = owl_shell_session:post_exprs( Session, Exprs ),
-
-					handle_io_data_process_token_queue( S1 );
+					handle_info_io_data_process_token_queue( S1 );
 				{error, {Line, erl_parse, ErrorIOL}} ->
+					%% TODO: Report syntax error into the session
 					io:format("\tError: (~p) ~s~n", [ Line, ErrorIOL ]),
-					handle_io_data_process_token_queue( S1 )
+					handle_info_io_data_process_token_queue( S1 )
 			end
 	end.
 
@@ -141,10 +129,6 @@ session_create_and_attach( TopSup ) ->
 	ok = owl_shell_session:attach( Session, self() ),
 	{ok, SID, Session}.
 
-io_write_prompt( S0 = #s{ io = IO, active_session_id = SID } ) ->
-	?io:write( IO, io_lib:format( "[~p session: ~p] # ", [ node(), SID ] ) ),
-	{ok, S0}.
-
 token_queue_get_expr( Q ) ->
 	token_queue_get_expr( Q, [] ).
 
@@ -157,4 +141,42 @@ token_queue_get_expr( Q, Acc ) ->
 		empty ->
 			error
 	end.
+
+
+
+io_write_prompt( S0 = #s{ io = IO, active_session_id = SID } ) ->
+	?io:write( IO, io_lib:format( "[~p session: ~p] # ", [ node(), SID ] ) ),
+	{ok, S0}.
+
+handle_cast_post_result( SID, RID, Result, S0 = #s{ io = IO }) ->
+	?io:write( IO, io_lib:format( "v(~p:~p) = ~p~n", [ SID, RID, Result ]) ),
+	{ok, S1} = io_write_prompt( S0 ),
+	{noreply, S1}.
+
+handle_cast_post_error( SID, RID, EvalProcessDead, Reason, S0 = #s{ io = IO } ) ->
+	?io:write( IO, io_lib:format( "v(~p:~p) : ~p terminated with exception~n", [ SID, RID, EvalProcessDead ] ) ),
+	?io:write( IO, io_lib:format( "\t~p~n", [ Reason ] ) ),
+	{ok, S1} = io_write_prompt( S0 ),
+	{noreply, S1}.
+
+handle_info_io_request( ReplyTo, ReplyAs, Request, S = #s{ io = IO } ) ->
+	% io:format("~p: Inbound IO-request: ~p~n", [ self(), Request ]),
+	case Request of
+		{ put_chars, Chars } ->
+				?io:write( IO, Chars );
+
+		{ put_chars, _Enc, Chars } ->
+			?io:write( IO, Chars );
+
+		{ put_chars, _Enc, M, F, A } ->
+			?io:write( IO, erlang:apply( M, F, A ) );
+
+		{ put_chars, M, F, A } ->
+			?io:write( IO, erlang:apply( M, F, A ) );
+
+		Unexpected ->
+			io:format("Unexpected IO-request: ~p~n", [Unexpected])
+	end,
+	_ = erlang:send( ReplyTo, {io_reply, ReplyAs, ok} ),
+	{noreply, S}.
 
